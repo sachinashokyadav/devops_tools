207094d1e60c581c18dc3a7379e5c60eef9b96d9
sachinashokyadav <sachin.ashok.yadav18@gmail.com>
2025-10-23 13:10:34 +0530

    Create variables.tf

---
+++ terraform/ec2/variables.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+  description = "value for ami"
+}
+
+variable "instance_type" {
+  description = "value for instance type"
+}
+
+variable "subnet_id" {
+  description = "value for subnet"
+}
+
+variable "key_name" {
+  description = "value for key name"
+}
